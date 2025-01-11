ac468cc7e5070eac7d7724161555d965c320e2db
zombie <claney2005@protonmail.com>
2025-01-11 13:15:39 -0600

    first commit

---
+++ src/main/kotlin/com/zombie/anomalous/Anomalous.kt
@@ -0,0 +1,19 @@
+package com.zombie.anomalous
+
+import xyz.xenondevs.nova.addon.Addon
+
+object Anomalous : Addon() {
+    
+    override fun init() {
+        // Called when the addon is initialized.
+    }
+    
+    override fun onEnable() {
+        // Called when the addon is enabled.
+    }
+    
+    override fun onDisable() {
+        // Called when the addon is disabled.
+    }
+    
+}
